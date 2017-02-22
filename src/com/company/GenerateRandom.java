cdd046ba9c821119f4fffce349041b8d9e602d3c
pejuron <pejuron@gmail.com>
2017-02-22 19:50:49 +0800

    Added minor changes:
    *Modified GenerateRandom class for code optimization
    
    v2.02
    
    sumitted by: JRP

--- src/com/company/GenerateRandom.java
+++ src/com/company/GenerateRandom.java
@@ -4,29 +4,25 @@ import java.util.Random;
 
 public class GenerateRandom {
 
+    static String[] randomBoys = { "Joru","Mark","Carl", "Julleane", "Bert" };
+    static String[] randomGirls = { "Grace","Louise","Kathleen", "Maria Ossawa", "Kathryn" };
+    static String[] randomRelations = { " hates "," loves "," is dating with ", " was having fun with "};
+
     static Random random = new Random();
+    static int randomIndex;
 
     private static String getRandomBoys() {
-
-        String[] randomBoys = { "Joru","Mark","Carl", "Julleane", "Bert" };
-
-        int randomIndex = random.nextInt((randomBoys.length-1)+1);
+        randomIndex = random.nextInt((randomBoys.length-1)+1);
         return randomBoys[randomIndex];
     }
 
     private static String getRandomGirls() {
-
-        String[] randomGirls = { "Grace","Louise","Kathleen", "Maria Ossawa", "Kathryn" };
-
-        int randomIndex = random.nextInt((randomGirls.length-1)+1);
+        randomIndex = random.nextInt((randomGirls.length-1)+1);
         return randomGirls[randomIndex];
     }
 
     private static String getRandomRelations() {
-
-        String[] randomRelations = { " hates "," loves "," is dating with ", " was having fun with "};
-
-        int randomIndex = random.nextInt((randomRelations.length-1)+1);
+        randomIndex = random.nextInt((randomRelations.length-1)+1);
         return randomRelations[randomIndex];
     }
 
