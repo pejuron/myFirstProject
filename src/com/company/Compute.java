1dcd8ea17f9b96e98ad01ef852fc3a90b3b969d0
pejuron <pejuron@gmail.com>
2017-02-22 18:43:59 +0800

    First Commit

---
+++ src/com/company/Compute.java
@@ -0,0 +1,53 @@
+package com.company;
+
+public class Compute {
+
+    public static int computeSum(int num){
+        int sum = 0;
+
+        for( int i = 1; i <= num; i++ ) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int factorial(int num){
+        int result = 1;
+
+        if ( num == 0 ) return 1;
+        else {
+            for( int i = num; i >= 1; i-- ) {
+                result *= i;
+            }
+        }
+        return result;
+    }
+
+    public static void evenGenerator(int num) {
+        int n = 0;
+        int[] evenNumbers = new int[num];
+
+        for ( int i = 1; i <= num*2; i++ ) {
+            if ( i%2 == 0 ) {
+                evenNumbers[n] = i;
+                n++;
+            }
+        }
+        for ( int i = 0; i < evenNumbers.length; i++ ) {
+            System.out.print(evenNumbers[i] + " ");
+        }
+    }
+
+    public static void fibonacci(int num){
+        int[] myFiboNum = new int[num];
+
+        for ( int i = 0; i < myFiboNum.length; i++ ) {
+            if ( i > 1 ) myFiboNum[i] = myFiboNum[i-2] + myFiboNum[i-1] ;
+            else myFiboNum[i] = i;
+        }
+
+        for ( int i = 0; i < myFiboNum.length; i++ ) {
+           System.out.print( myFiboNum[i] + " ");
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        System.out.println("While Loop:");
+        while( i < 11 ) System.out.print("[" + i++ +"]");
+
+        i = 1;
+        System.out.println("\nDo-While Loop:");
+        do System.out.print("[" + i++ +"]");
+        while( i < 11 );
+
+        System.out.println("\nFor Loop:");
+        for( i = 1; i < 11; i++ )
+            System.out.print("[" + i +"]");
+
+        System.out.println("\n\nSum of Loops:");
+        System.out.println(Compute.computeSum(6));
+
+        System.out.println("\nFactorial:");
+        System.out.println(Compute.factorial(7));
+
+        System.out.println("\nEven number generator:");
+        Compute.evenGenerator(8);
+
+        System.out.println("\n\nFibonacci:");
+        Compute.fibonacci(9);
+    }
+}
